2eb89adf4972f8900093e9367f68bda97c489814
Tiago Moreira <tiago10moreira1@gmail.com>
2021-06-13 03:54:08 -0300

    feat: create basic activities

---
+++ app/src/main/java/com/example/quebragalho/FinishServiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.quebragalho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinishServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_service);
+    }
+}
---
+++ app/src/main/java/com/example/quebragalho/ManageServiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.quebragalho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manage_service_activity);
+    }
+}
