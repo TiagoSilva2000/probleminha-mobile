cc862291cb3d5875099f7bd7524074998b93465a
Tiago Moreira <tiago10moreira1@gmail.com>
2021-06-13 17:30:23 -0300

    feat(layout): add more basic layout structure

--- app/src/main/java/com/example/quebragalho/ServiceViewActivity.java
+++ app/src/main/java/com/example/quebragalho/ServiceViewActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class ServiceViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_main);
+        setContentView(R.layout.activity_service_view);
     }
 }
